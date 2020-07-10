33d12f1af49eab9091c3395600197a2c5a4d8282
rutaks <rootsum.dev@gmail.com>
2020-07-10 07:26:46 +0200

    Setup local test data

---
+++ tempData.js
@@ -0,0 +1,57 @@
+export const tempData = [
+  {
+    id: 1,
+    name: "Plan a trip",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Book a flight 1",
+        completed: true,
+      },
+      {
+        title: "Book a flight 2",
+        completed: false,
+      },
+      {
+        title: "Book a flight 3",
+        completed: true,
+      },
+    ],
+  },
+  // {
+  //   name: "Plan a trip 2",
+  //   color: "#8022D9",
+  //   todos: [
+  //     {
+  //       title: "Book a flight 4",
+  //       completed: false,
+  //     },
+  //     {
+  //       title: "Book a flight 5",
+  //       completed: false,
+  //     },
+  //     {
+  //       title: "Book a flight 6",
+  //       completed: true,
+  //     },
+  //   ],
+  // },
+  // {
+  //   name: "Plan a trip 3",
+  //   color: "#EBA834",
+  //   todos: [
+  //     {
+  //       title: "Book a flight 7",
+  //       completed: true,
+  //     },
+  //     {
+  //       title: "Book a flight 8",
+  //       completed: false,
+  //     },
+  //     {
+  //       title: "Book a flight 9",
+  //       completed: false,
+  //     },
+  //   ],
+  // },
+];
